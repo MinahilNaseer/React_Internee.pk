3e55a50e70bbdc2973d731acfebdded06bac5802
Minahil Naseer <01-131212-018@student.bahria.edu.pk>
2025-02-03 20:13:02 +0500

    secure

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCG3tw4qYhkUdy_8oz-AUVojRFb8qaa6Dc",
-  authDomain: "interneeweb.firebaseapp.com",
-  projectId: "interneeweb",
-  storageBucket: "interneeweb.firebasestorage.app",
-  messagingSenderId: "926466493293",
-  appId: "1:926466493293:web:038d259462dc1d9d14e864",
-  measurementId: "G-HSK63HFQB0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
